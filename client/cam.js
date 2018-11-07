9fb31b4c00b962d8b92d495625349311cfcac461
mster0103 <matthew.sternquist@gmail.com>
2018-11-07 15:45:35 -0800

    modified webcam

---
+++ client/cam.js
@@ -0,0 +1,106 @@
+(env => {
+  const ML_API_URL = "???";
+
+  navigator.getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
+
+  function getPrediction(imageDataUrl) {
+    return fetch(`${ML_API_URL}`, {
+      method: "POST",
+      body: imageDataUrl
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        return data;
+      });
+  }
+
+  function captureVideoFrame(canvas) {
+    return canvas.toDataURL("image/jpeg");
+  }
+
+  function clearCanvas(canvas) {
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, c.height);
+  }
+
+  function drawRect(canvas, x, y, width, height, color = "#FF0000") {
+    const ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.globalAlpha = 1.0;
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = color;
+    ctx.strokeRect(x, y, width, height);
+  }
+
+  /**
+   * Main app things
+   */
+  function App() {
+    const videoElem = document.querySelector("#video-container video");
+    const captionElem = document.querySelector(".caption");
+    const canvas = document.querySelector("#video-container > canvas");
+
+    /**
+     * Stateful things
+     */
+    const state = {
+      predictionLoop: undefined,
+      captionText: "?"
+    };
+
+    const setCaptionText = text => {
+      state.captionText = text;
+      captionElem.textContent = state.captionText;
+    };
+
+    const startPredictionLoop = () => {
+      state.predictionLoop = setInterval(() => {
+        const imageDataUrl = captureVideoFrame(canvas);
+        getPrediction(imageDataUrl)
+          .then(data => {
+            clearCanvas(canvas);
+          })
+          .catch(() => {
+            stopPredictionLoop();
+          });
+      }, 200);
+    };
+
+    const stopPredictionLoop = () => {
+      clearInterval(state.predictionLoop);
+    };
+
+    const initialise = () => {
+      navigator.getUserMedia(
+        {
+          video: true
+        },
+        stream => {
+          console.info("got stream", stream);
+          videoElem.srcObject = stream;
+
+          // configure a fake presentation for demo purposes
+          if (env === "demo") {
+          } else {
+            startPredictionLoop();
+          }
+        },
+        err => console.error(err)
+      );
+    };
+
+    return {
+      initialise
+    };
+  }
+
+  const app = App();
+  app.initialise();
+})("demo");
