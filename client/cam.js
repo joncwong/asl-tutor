22408acf9cd78ed39efc0ceee07bf3ca4f1a30a9
mster0103 <matthew.sternquist@gmail.com>
2018-12-02 22:33:25 -0800

    Woops

--- client/cam.js
+++ client/cam.js
@@ -69,7 +69,7 @@
         getPrediction(imageDataUrl)
           .then(data => {
             clearCanvas(canvas);
-            setCaptionText(data.prediction);
+            state.captionText = data.prediction;
             captionElem.textContent = state.captionText;
           })
           .catch(() => {
