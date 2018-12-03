af229eaba34d30879f04ad60a46fd532689c9507
mster0103 <matthew.sternquist@gmail.com>
2018-12-02 21:41:18 -0800

    fixed predict text

--- client/cam.js
+++ client/cam.js
@@ -45,7 +45,8 @@
    * Main app things
    */
   function App() {
-    const videoElem = document.querySelector("#video-container video");
+    const videoElem = document.querySelector
+    ("#video-container video");
     const captionElem = document.querySelector(".caption");
     const canvas = document.querySelector("#video-container > canvas");
 
@@ -69,6 +70,7 @@
           .then(data => {
             clearCanvas(canvas);
             setCaptionText(data.prediction);
+            captionElem.textContent = state.captionText;
           })
           .catch(() => {
             stopPredictionLoop();
